d049a96d8211253c42547227970f42994e2a0f63
yassin <yassin.hajaj@hotmail.com>
2021-03-18 20:40:06 +0100

    Included GreetingEntity inside of the GreetingResource

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,5 +1,8 @@
 package org.acme;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.Entity;
 import javax.transaction.Transactional;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -17,5 +20,19 @@ public class GreetingResource {
         greetingEntity.setMessage("hello !");
         greetingEntity.persistAndFlush();
         return greetingEntity.getMessage();
+    }
+
+    @Entity
+    public static class GreetingEntity extends PanacheEntity {
+
+        private String message;
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
     }
 }
