88bb291c585aeb4eb1f250973abd27ca51c3a30a
yassin <yassin.hajaj@hotmail.com>
2021-03-18 20:48:12 +0100

    Making the test check the entities count in database

--- src/test/java/org/acme/GreetingResourceTest.java
+++ src/test/java/org/acme/GreetingResourceTest.java
@@ -2,6 +2,8 @@ package org.acme;
 
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
+import org.acme.GreetingResource.GreetingEntity;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -13,11 +15,15 @@ public class GreetingResourceTest {
 
     @Test
     public void testHelloEndpoint() {
+        Assertions.assertEquals(0, GreetingEntity.count());
+
         given()
                 .when().get("/hello")
                 .then()
                 .statusCode(200)
                 .body(is("hello !"));
+
+        Assertions.assertEquals(1, GreetingEntity.count());
     }
 
 }
